07fb1aea45471ca8374e2dc432d3a8e140de98bb
MMurphy98 <mjg654654@outlook.com>
2022-08-20 11:38:01 +0800

    Update 202208

--- Test.py
+++ Test.py
@@ -1,9 +1,24 @@
-print("Hello world!")
+import numpy as np
+from scipy import signal
 
-print("Gitlens+")
 
-print("check update!")
+def strideConv(arr, arr2, s):
+    return signal.convolve2d(arr, arr2[::-1, ::-1], mode='valid')[::s, ::s]
 
-for i in range(1,10):
-    print(i)
-    print(i+1)
+
+if __name__ == '__main__':
+    k_size = 3
+    im_size = 4
+
+    kernel = np.arange(0, k_size * k_size, 1).reshape((k_size, k_size))
+    act_map = np.arange(0, im_size * im_size, 1).reshape((im_size, im_size))
+    conv = strideConv(act_map, kernel, 1)
+
+    print("Kernel:")
+    print(kernel)
+
+    print("act_map:")
+    print(act_map)
+
+    print("conv:")
+    print(conv)
